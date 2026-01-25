ece7abdeae716724409a8efb5867e8cc90710fcd
Amos Glenn <amos@amosglenn.com>
2026-01-24 21:54:01 -0500

    removing old files for fresh start

--- src/components/flowbite/index.ts
+++ src/components/flowbite/index.ts
@@ -12,21 +12,11 @@
 export { default as Footer } from "./Footer.astro";
 export { default as Header } from "./Header.astro";
 
-// Hero Components
-export { default as HeroCarousel } from "./HeroCarousel.astro";
-
-// Interactive Components
-export { default as Carousel } from "./Carousel.astro";
-
 // Feature Components
 export { default as FeatureAlternate } from "./FeatureAlternate.astro";
-
-// Contact Components
-export { default as ContactForm } from "./ContactForm.astro";
 
 // Re-export types for convenience
 export type { FeatureItem } from "./FeatureAlternate.astro";
 export type { FooterColumn, FooterLink, SocialLink } from "./Footer.astro";
-export type { NavLink } from "./Header.astro";
-export type { Slide } from "./HeroCarousel.astro";
+export type { NavLink } from './Header.astro';
 
